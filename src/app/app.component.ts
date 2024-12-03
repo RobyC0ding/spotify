7bbe1dee475406a96821739de007bcb06a8a804a
Roberto <roberto.zuccaro@itsincom.it>
2024-12-03 14:49:51 +0100

    impostazione funzioni ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {DUMMY_USERS} from './dummy-users';
+import {CommonModule} from '@angular/common';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [SongComponent, CommonModule, PlaylistComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'spotify';
+  users = DUMMY_USERS;
+  selectedUserId!: string;
+
+  get selectedPlaylist() {
+    let p = this.playlists.find(p => p.id === this.selectedPlaylistId);
+    return this.users.find(playlist => playlist.id === this.selectedUserId);
+  }
+
+  onSelectPlaylist(id: string) {
+    this.selectedPlaylistId = id;
+    console.log("User selected id: " + id);
+  }
 }
